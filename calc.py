7a4ea61cd6c0674b1a737e50d8d52744aaab87e5
DanielBThornqvist <danielbthornqvist@gmail.com>
2014-10-01 11:53:24 +0200

    plusminusggrdelat

--- calc.py
+++ calc.py
@@ -1,17 +1,46 @@
 # -*- coding: cp1252 -*-
-def val(a):
+
+def val():
     print "Välj om du vill 1: Addera, 2: Subtrahera, 3: Multiplicera, 4: Dividera"
     val = raw_input("Skriv nummer här: ")
-    if val == 1:
+    if val == "1":
         print "Du har valt addera"
-    elif val == 2:
+        addera()
+    elif val == "2":
         print "Du har valt subtrahera"
-    elif val == 3:
+        subtrahera()
+    elif val == "3":
         print "Du har valt multiplicera"
-    elif val == 4:
+        multiplicera()
+    elif val == "4":
         print "Du har valt dividera"
+        dividera()
     else:
         print "Error"
-        break
+        
 
-def addera 
+def addera():
+    plus1 = int(raw_input("Skriv första siffran: "))
+    plus2 = int(raw_input("Skriv andra siffran: "))
+    svar = plus1 + plus2
+    print "Svaret är ", svar
+
+def subtrahera():
+    minus1 = int(raw_input("Skriv första siffran: "))
+    minus2 = int(raw_input("Skriv andra siffran: "))
+    svar = minus1 - minus2
+    print "Svaret är ", svar
+
+def multiplicera():
+    ggr1 = int(raw_input("Skriv första siffran: "))
+    ggr2 = int(raw_input("Skriv andra siffran: "))
+    svar = ggr1 * ggr2
+    print "Svaret är ", svar
+
+def dividera():
+    dvd1 = int(raw_input("Skriv första siffran: "))
+    dvd2 = int(raw_input("Skriv andra siffran: "))
+    svar = dvd1 / dvd2
+    print "Svaret är ", svar
+
+val()
