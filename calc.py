277293b071108fc9b764d960c00831d9a77e63d9
DanielBThornqvist <danielbthornqvist@gmail.com>
2014-10-01 11:30:35 +0200

    lagt till meny

--- calc.py
+++ calc.py
@@ -2,4 +2,16 @@
 def val(a):
     print "Välj om du vill 1: Addera, 2: Subtrahera, 3: Multiplicera, 4: Dividera"
     val = raw_input("Skriv nummer här: ")
-    
+    if val == 1:
+        print "Du har valt addera"
+    elif val == 2:
+        print "Du har valt subtrahera"
+    elif val == 3:
+        print "Du har valt multiplicera"
+    elif val == 4:
+        print "Du har valt dividera"
+    else:
+        print "Error"
+        break
+
+def addera 
