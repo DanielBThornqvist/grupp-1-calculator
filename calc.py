5b8edb310f83cef94baf8c43dbb366024cb34cff
DanielBThornqvist <danielbthornqvist@gmail.com>
2014-10-02 10:37:51 +0200

    fixat median
    behöde inte loop

--- calc.py
+++ calc.py
@@ -63,30 +63,28 @@ def median():
     talmedian = eval(raw_input("Skriv en array ex, [1, 2, 3, 4]"))
     sortera = sorted(talmedian)
     if len(sortera)%2 != 0:
-        for i in sortera:
-            if i > (len(sortera)/2):
-                svar = i - 1
-                print svar
+        svar = sortera[len(sortera)/2]
+        print svar
     else:
-        for i in sortera:
-            if i > (len(sortera)/2):
-                svar = float(((i + (i - 1)) / 2))
-                print svar
+        var1 = float(sortera[len(sortera)/2])
+        var2 = float((sortera[len(sortera)/2-1]))
+        svar = float(((var1 + var2) / 2))
+        print svar
 
 def summafler():
     summafler = eval(raw_input("Skriv de tal du vill addera som en array ex. [5, 10, 15]"))
     svar = float (sum(summafler))
     print "Svaret är ", svar
 
-def upphojt():
-    upp1 = int(raw_input("Skriv basen: "))
-    upp2 = int(raw_input("Skriv vad du vill höja upp den till: "))
-    elif upp2 == 0:
-        print 1
-    elif upp2 == 1:
-        print upp1
-    elif upp2 > 1
-    svar = upp1 * 
-    print "Svaret är ", svar
+##def upphojt():
+##    upp1 = int(raw_input("Skriv basen: "))
+##    upp2 = int(raw_input("Skriv vad du vill höja upp den till: "))
+##    elif upp2 == 0:
+##        print 1
+##    elif upp2 == 1:
+##        print upp1
+##    elif upp2 > 1
+##    svar = upp1 * 
+##    print "Svaret är ", svar
 
 val()
